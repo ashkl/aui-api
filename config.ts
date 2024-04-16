0008cbdec5527f1cde994cf62a6b7bd2734ecefb
ashkl <asshwin12@yahoo.co.uk>
2024-04-16 19:50:53 +0100

    update config for truenas

--- config.ts
+++ config.ts
@@ -10,14 +10,6 @@ dotenv.config({ path: path.resolve(__dirname, "./.env.local") });
 
 interface ENV {
   PORT: number | undefined;
-  
-  HOME_ASSISTANT_URL: string | undefined;
-  JELLYFIN_URL: string | undefined;
-  JOIN_API_URL: string | undefined;
-
-  MAPBOX_KEY: string | undefined;
-  HOME_ASSISTANT_KEY: string | undefined;
-  JELLYFIN_KEY: string | undefined;
 
   //PROD PROXMOX
   PROD_URL: string | undefined;
@@ -26,19 +18,27 @@ interface ENV {
   //NAS PROXMOX
   NAS_URL: string | undefined;
   NAS_KEY: string | undefined;
+
+  //TRUENAS
+  TRUENAS_URL: string | undefined;
+  TRUENAS_KEY: string | undefined;
+
+  //HOME ASSISTANT
+  HOME_ASSISTANT_URL: string | undefined;
+  HOME_ASSISTANT_KEY: string | undefined;
+
+  //JELLYFIN
+  JELLYFIN_URL: string | undefined;
+  JOIN_API_URL: string | undefined;
+  JELLYFIN_KEY: string | undefined;
+  
+  //EXTRA
+  MAPBOX_KEY: string | undefined;
 }
 
 interface Config {
   PORT: number | undefined;
 
-  HOME_ASSISTANT_URL: string | undefined;
-  JELLYFIN_URL: string | undefined;
-  JOIN_API_URL: string | undefined;
-
-  MAPBOX_KEY: string | undefined;
-  HOME_ASSISTANT_KEY: string | undefined;
-  JELLYFIN_KEY: string | undefined;
-
   //PROD PROXMOX
   PROD_URL: string | undefined;
   PROD_KEY: string | undefined;
@@ -46,6 +46,22 @@ interface Config {
   //NAS PROXMOX
   NAS_URL: string | undefined;
   NAS_KEY: string | undefined;
+
+  //TRUENAS
+  TRUENAS_URL: string | undefined;
+  TRUENAS_KEY: string | undefined;
+
+  //HOME ASSISTANT
+  HOME_ASSISTANT_URL: string | undefined;
+  HOME_ASSISTANT_KEY: string | undefined;
+
+  //JELLYFIN
+  JELLYFIN_URL: string | undefined;
+  JOIN_API_URL: string | undefined;
+  JELLYFIN_KEY: string | undefined;
+  
+  //EXTRA
+  MAPBOX_KEY: string | undefined;
 }
 
 // Loading process.env as ENV interface
@@ -53,14 +69,6 @@ interface Config {
 const getConfig = (): ENV => {
   return {
     PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
-    
-    HOME_ASSISTANT_URL: process.env.HOME_ASSISTANT_URL,
-    JELLYFIN_URL: process.env.JELLYFIN_URL,
-    JOIN_API_URL: process.env.JOIN_API_URL,
-
-    MAPBOX_KEY: process.env.MAPBOX_KEY,
-    HOME_ASSISTANT_KEY: process.env.HOME_ASSISTANT_KEY,
-    JELLYFIN_KEY: process.env.JELLYFIN_KEY,
 
     //PROD PROXMOX
     PROD_URL: process.env.PROD_URL,
@@ -69,6 +77,22 @@ const getConfig = (): ENV => {
     //NAS PROXMOX
     NAS_URL: process.env.NAS_URL,
     NAS_KEY: process.env.NAS_KEY,
+
+    //TRUENAS
+    TRUENAS_URL: process.env.TRUENAS_URL,
+    TRUENAS_KEY: process.env.TRUENAS_KEY,
+
+    //HOME ASSISTANT
+    HOME_ASSISTANT_URL: process.env.HOME_ASSISTANT_URL,
+    HOME_ASSISTANT_KEY: process.env.HOME_ASSISTANT_KEY,
+
+    //JELLYFIN
+    JELLYFIN_URL: process.env.JELLYFIN_URL,
+    JOIN_API_URL: process.env.JOIN_API_URL,
+    JELLYFIN_KEY: process.env.JELLYFIN_KEY,
+
+    //EXTRA
+    MAPBOX_KEY: process.env.MAPBOX_KEY,
   };
 };
 
